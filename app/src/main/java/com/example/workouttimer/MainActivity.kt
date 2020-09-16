8d15e5a513602460dca65744daae8871db64c6b8
frichards17 <frank.richards10@gmail.com>
2020-09-16 18:34:31 +0100

    Added basic work and rest values which UI affects

--- app/src/main/java/com/example/workouttimer/MainActivity.kt
+++ app/src/main/java/com/example/workouttimer/MainActivity.kt
@@ -2,10 +2,96 @@ package com.example.workouttimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var work = 0
+    var rest = 0
+    var sets = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setClickListeners()
+
+
+
+
     }
+
+    private fun setClickListeners(): Unit{
+        work_minus.setOnClickListener {
+            updateWork()
+            when {
+                work < 1 -> println("Work is already 0")
+                work < 10 -> work = 0
+                else -> work -= 10
+            }
+            val workMins = work / 60
+            val workSecs = work % 60
+            work_minutes.setText(workMins.toString().padStart(2,'0'))
+            work_seconds.setText(workSecs.toString().padStart(2,'0'))
+        }
+
+        work_add.setOnClickListener {
+            updateWork()
+            when {
+                work < 5989 -> work += 10
+                else -> work = 5999
+            }
+            val workMins = work / 60
+            val workSecs = work % 60
+            work_minutes.setText(workMins.toString().padStart(2,'0'))
+            work_seconds.setText(workSecs.toString().padStart(2,'0'))
+        }
+
+        rest_minus.setOnClickListener {
+            updateRest()
+            when {
+                rest < 1 -> println("Work is already 0")
+                rest < 10 -> rest = 0
+                else -> rest -= 10
+            }
+            val restMins = rest / 60
+            val restSecs = rest % 60
+            rest_minutes.setText(restMins.toString().padStart(2,'0'))
+            rest_seconds.setText(restSecs.toString().padStart(2,'0'))
+        }
+
+        rest_add.setOnClickListener {
+            updateRest()
+            when {
+                rest < 5989 -> rest += 10
+                else -> rest = 5999
+            }
+            val restMins = rest / 60
+            val restSecs = rest % 60
+            rest_minutes.setText(restMins.toString().padStart(2,'0'))
+            rest_seconds.setText(restSecs.toString().padStart(2,'0'))
+        }
+    }
+
+    private fun updateWork(): Unit{
+        var mins = work_minutes.text.toString()
+        var secs = work_seconds.text.toString()
+        if (mins.isBlank()) mins = "0"
+        if (secs.isBlank()) secs = "0"
+        val minsInt = Integer.parseInt(mins)
+        val secsInt = Integer.parseInt(secs)
+        work = (60 * minsInt) + secsInt
+
+    }
+
+    private fun updateRest(): Unit{
+        var mins = rest_minutes.text.toString()
+        var secs = rest_seconds.text.toString()
+        if (mins.isBlank()) mins = "0"
+        if (secs.isBlank()) secs = "0"
+        val minsInt = Integer.parseInt(mins)
+        val secsInt = Integer.parseInt(secs)
+        rest = (60 * minsInt) + secsInt
+    }
+
 }
