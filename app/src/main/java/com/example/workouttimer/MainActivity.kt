96c43f308b7385ce90486ce8e50ff19be895bb96
frichards17 <frank.richards10@gmail.com>
2020-09-23 22:38:36 +0100

    added reset button

--- app/src/main/java/com/example/workouttimer/MainActivity.kt
+++ app/src/main/java/com/example/workouttimer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.workouttimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,81 +15,86 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setClickListeners()
-
-
-
-
     }
 
-    private fun setClickListeners(): Unit{
-        work_minus.setOnClickListener {
-            updateWork()
-            when {
-                work < 1 -> println("Work is already 0")
-                work < 10 -> work = 0
-                else -> work -= 10
-            }
-            val workMins = work / 60
-            val workSecs = work % 60
-            work_minutes.setText(workMins.toString().padStart(2,'0'))
-            work_seconds.setText(workSecs.toString().padStart(2,'0'))
+    fun minusWork(view: View){
+        updateWork()
+        when {
+            work < 1 -> println("Work is already 0")
+            work < 10 -> work = 0
+            else -> work -= 10
         }
+        val workMins = work / 60
+        val workSecs = work % 60
+        work_minutes.setText(workMins.toString().padStart(2,'0'))
+        work_seconds.setText(workSecs.toString().padStart(2,'0'))
+    }
 
-        work_add.setOnClickListener {
-            updateWork()
-            when {
-                work < 5989 -> work += 10
-                else -> work = 5999
-            }
-            val workMins = work / 60
-            val workSecs = work % 60
-            work_minutes.setText(workMins.toString().padStart(2,'0'))
-            work_seconds.setText(workSecs.toString().padStart(2,'0'))
+    fun addWork(view: View){
+        updateWork()
+        when {
+            work < 5989 -> work += 10
+            else -> work = 5999
         }
+        val workMins = work / 60
+        val workSecs = work % 60
+        work_minutes.setText(workMins.toString().padStart(2,'0'))
+        work_seconds.setText(workSecs.toString().padStart(2,'0'))
+    }
 
-        rest_minus.setOnClickListener {
-            updateRest()
-            when {
-                rest < 1 -> println("Work is already 0")
-                rest < 10 -> rest = 0
-                else -> rest -= 10
-            }
-            val restMins = rest / 60
-            val restSecs = rest % 60
-            rest_minutes.setText(restMins.toString().padStart(2,'0'))
-            rest_seconds.setText(restSecs.toString().padStart(2,'0'))
+    fun minusRest(view: View){
+        updateRest()
+        when {
+            rest < 1 -> println("Work is already 0")
+            rest < 10 -> rest = 0
+            else -> rest -= 10
         }
+        val restMins = rest / 60
+        val restSecs = rest % 60
+        rest_minutes.setText(restMins.toString().padStart(2,'0'))
+        rest_seconds.setText(restSecs.toString().padStart(2,'0'))
+    }
 
-        rest_add.setOnClickListener {
-            updateRest()
-            when {
-                rest < 5989 -> rest += 10
-                else -> rest = 5999
-            }
-            val restMins = rest / 60
-            val restSecs = rest % 60
-            rest_minutes.setText(restMins.toString().padStart(2,'0'))
-            rest_seconds.setText(restSecs.toString().padStart(2,'0'))
+    fun addRest(view: View){
+        updateRest()
+        when {
+            rest < 5989 -> rest += 10
+            else -> rest = 5999
         }
+        val restMins = rest / 60
+        val restSecs = rest % 60
+        rest_minutes.setText(restMins.toString().padStart(2,'0'))
+        rest_seconds.setText(restSecs.toString().padStart(2,'0'))
+    }
 
-        sets_minus.setOnClickListener {
-            updateSets()
-            when{
-                sets < 1 -> println("Sets is already 1")
-                else -> sets -= 1
-            }
-            sets_txt.setText(sets.toString().padStart(2, '0'))
+    fun minusSets(view: View){
+        updateSets()
+        when{
+            sets < 1 -> println("Sets is already 1")
+            else -> sets -= 1
         }
+        sets_txt.setText(sets.toString().padStart(2, '0'))
+    }
 
-        sets_add.setOnClickListener {
-            updateSets()
-            when{
-                sets < 99 -> sets += 1
-                else -> sets = 99
-            }
-            sets_txt.setText(sets.toString().padStart(2, '0'))
+    fun addSets(view: View){
+        updateSets()
+        when{
+            sets < 99 -> sets += 1
+            else -> sets = 99
         }
+        sets_txt.setText(sets.toString().padStart(2, '0'))
+    }
+
+    fun reset(view: View){
+        work = 0
+        rest = 0
+        sets = 0
+        work_minutes.text = null
+        work_seconds.text = null
+        rest_minutes.text = null
+        rest_seconds.text = null
+        sets_txt.text = null
+
     }
 
     private fun updateWork(): Unit{
